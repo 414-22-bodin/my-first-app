e68b8b35d93a9d46023113ca105976ebab5cf09b
414-22-bodin <54670@yupparaj.ac.th>
2026-08-17 14:16:06 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -84,4 +84,4 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2, ans3, ans4)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นายบดินทร์ ปาระมีแจ้ ม.4/14 เลขที่ 22")
